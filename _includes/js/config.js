6f64a083b53ad3430bf594783d0785ece1e54144
Tim Robertson <timrobertson100@gmail.com>
2021-01-22 17:05:11 +0100

    Update config.js
    Correct config for scope (other change still needed elsewhere for this to work)

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -2,9 +2,9 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
   primary: themeStyle.colors.primary
 }});
 
-var siteConfig = { 
-  "type": "and", "predicates": [
- { "type": "equals", "key": "NETWORK_KEY", "value": "99d66b6c-9087-452f-a9d4-f15f2c2d0e7e"},
- { "type": "equals", "key": "TAXON_KEY", "value": "44"}
- ]
+var siteConfig = {
+  rootPredicate: { "type": "and", "predicates": [
+     { "type": "equals", "key": "networkKey", "value": "99d66b6c-9087-452f-a9d4-f15f2c2d0e7e"},
+     { "type": "equals", "key": "taxonKey", "value": "44"}
+  ]}
 };
