4681cebde71cc6a6a3911b3eb0d1b4aadd8dd54a
Morten Høfft <mhoefft@gbif.org>
2022-03-09 14:23:05 +0100

    feat: update configuration schema. See https://github.com/gbif/hosted-portals/issues/221

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,12 +1,25 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary
+  }
+});
 
 var siteConfig = {
-  rootPredicate: { "type": "and", "predicates": [
-     { "type": "equals", "key": "networkKey", "value": "99d66b6c-9087-452f-a9d4-f15f2c2d0e7e"},
-     { "type": "equals", "key": "taxonKey", "value": "44"}
-  ]},
-  highlightedFilters: ['basisOfRecord','taxonKey','gadmGid','institutionCode','year' ],
-  excludedFilters: ['protocol']
+  routes: {
+    occurrenceSearch: {
+      // The route you are currently using for occurrence search. The language prefix will be added automatically
+      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
+      route: '/data'
+    }
+  },
+  occurrence: {
+    rootPredicate: {
+      "type": "and", "predicates": [
+        { "type": "equals", "key": "networkKey", "value": "99d66b6c-9087-452f-a9d4-f15f2c2d0e7e" },
+        { "type": "equals", "key": "taxonKey", "value": "44" }
+      ]
+    },
+    highlightedFilters: ['basisOfRecord', 'taxonKey', 'gadmGid', 'institutionCode', 'year'],
+    excludedFilters: ['protocol']
+  }
 };
